e85c7eeb5101e55fd94fea2137f7d5eba2c61032
Jason Njoku <JasNjoku@gmail.com>
2023-04-01 16:57:54 +0100

    created add cards logic

--- personello/src/Components/List.js
+++ personello/src/Components/List.js
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
+import { useParams } from "react-router-dom";
 
 
 function List(props) {
-    const [cards, setCards] = useState([]);
+    const { id } = useParams();
+    let board = JSON.parse(localStorage.getItem('boards')).find((x) => x.name === id);
+    let list = board.lists[props.listId]
+    const [cards, setCards] = useState(list.listCards);
     const [cardName, setCardName] = useState('');
+    // const list = board.lists[props.listId];
+    // let cardItems = list.listCards;
+
+    // useEffect(() => {
+    //     console.log(cards)
+    // })
 
     const dragStart = (e) => {
         e.target.classList.add('dragging')
@@ -19,25 +29,40 @@ function List(props) {
     }
 
     const createCard = () => {
-        if(cardName.length > 3) {
-            setCards(prevState => [...prevState, cardName])
+        let card = { name: '', todos: [] }
+
+
+        if (cardName.length >= 3) {
+            card.name = cardName;
+            board.lists[props.listId].listCards.push(card)
+            const allBoards = JSON.parse(localStorage.getItem('boards'));
+
+            allBoards.forEach((element, index) => {
+                if (element.name === board.name) {
+                    allBoards.splice(index, 1, board)
+                }
+            })
+            
+            localStorage.setItem('boards', `${JSON.stringify(allBoards)}`)
+            setCards(prevState => [...prevState, card])
         }
+
     }
 
     return (
-        <div 
-            className="list" 
-            draggable="true" 
+        <div
+            className="list"
+            draggable="true"
             key={props.index}
             onDragStart={dragStart}
             onDragEnd={dragEnd}
         >
             <p>{props.name}</p>
-            
+
             <div className="list__card-items">
-                <div className={`.add-card.${props.index}`}>
-                    {cards.map(card => <Card cardName={card}/>)}
-                    <input onChange={(e) => {setCardName(e.target.value)}} placeholder="Enter card name..."/>
+                <div className={`.add-card.${props.listId}`}>
+                    {cards.map((card, index) => <Card card={card} key={index} />)}
+                    <input onChange={(e) => { setCardName(e.target.value) }} placeholder="Enter card name..." />
                     <button onClick={createCard}>Create Card</button>
                 </div>
                 <button onClick={showHidden}>Add a card</button>
