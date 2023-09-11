d4785201ba996674ef5c053f1f78ba99d0eb2cc9
Jason Njoku <JasNjoku@gmail.com>
2023-09-11 17:23:05 +0100

    update

--- personello/src/App.js
+++ personello/src/App.js
@@ -1,12 +1,12 @@
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider
 } from "react-router-dom"
 
 import Home from "./Routes/Home"
 import BoardComponent from "./Routes/Boards"
 //Router for the two pages, Home and Boards
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: (
